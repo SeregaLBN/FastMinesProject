0d8978e8b37a29214b48cea81bf7ad9484ffc0cd
SeregaLBN <SeregaLBN@gmail.com>
2014-08-14 23:23:54 +0300

    fix colors in mine image

--- swing_FastMines/src/swing/ua/ksn/fmg/view/swing/res/img/Mine.java
+++ swing_FastMines/src/swing/ua/ksn/fmg/view/swing/res/img/Mine.java
@@ -5,6 +5,8 @@ import java.awt.Graphics;
 
 import javax.swing.Icon;
 
+import ua.ksn.swing.geom.Cast;
+
 /** mine image */
 public class Mine implements Icon {
 	private Logo _logo;
@@ -14,6 +16,8 @@ public class Mine implements Icon {
 		_logo.setMargin(10);
 		_logo.setZoomX(0.7);
 		_logo.setZoomY(0.7);
+		for (int i = 0; i<_logo.Palette.length; i++)
+			_logo.Palette[i] = Cast.toColor(Cast.toColor(_logo.Palette[i]).darker(0.5));
 	}
 
 	public int getIconWidth() {
