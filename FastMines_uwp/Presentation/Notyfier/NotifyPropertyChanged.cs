98273c9e7a3638b3c6e9add65a82d1f8e861b0a2
SeregaLBN <SeregaLBN@gmail.com>
2015-11-03 14:15:00 +0200

    C#: moving / renaming presentation packages / classes  (part 2)

--- FastMines_uwp/Presentation/Notyfier/NotifyPropertyChanged.cs
+++ FastMines_uwp/Presentation/Notyfier/NotifyPropertyChanged.cs
@@ -1,68 +1,63 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace FastMines.Common {
+namespace FastMines.Presentation.Notyfier
+{
    /// <summary> Implementation of <see cref="INotifyPropertyChanged"/> to simplify models. </summary>
    [Windows.Foundation.Metadata.WebHostHidden]
-   public abstract class BindableBase : INotifyPropertyChangedEx {
+   public abstract class NotifyPropertyChanged : INotifyPropertyChanged
+   {
 
-      /// <summary>
-      /// Multicast event for property change notifications.
-      /// </summary>
+      /// <summary> Multicast event for property change notifications. </summary>
       public event PropertyChangedEventHandler PropertyChanged;
-      public event PropertyChangedExEventHandler PropertyChangedEx;
 
-      /// <summary>
-      /// Checks if a property already matches a desired value.  Sets the property and
-      /// notifies listeners only when necessary.
-      /// </summary>
+      /// <summary> Checks if a property already matches a desired value.  Sets the property and notifies listeners only when necessary. </summary>
       /// <typeparam name="T">Type of the property.</typeparam>
       /// <param name="storage">Reference to a property with both getter and setter.</param>
       /// <param name="value">Desired value for the property.</param>
-      /// <param name="propertyName">Name of the property used to notify listeners.  This
-      /// value is optional and can be provided automatically when invoked from compilers that
-      /// support CallerMemberName.</param>
-      /// <returns>True if the value was changed, false if the existing value matched the
-      /// desired value.</returns>
-      protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] String propertyName = null) {
+      /// <param name="propertyName">Name of the property used to notify listeners.  This value is optional and can be provided automatically
+      /// when invoked from compilers that support CallerMemberName.</param>
+      /// <returns>True if the value was changed, false if the existing value matched the desired value.</returns>
+      protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+      {
          if (object.Equals(storage, value)) return false;
 
          SetPropertyForce(ref storage, value, propertyName);
          return true;
       }
 
-      /// <summary>
-      /// Sets the property and notifies listeners only when necessary. (!! Without comparing by Equals)
-      /// </summary>
+      /// <summary> Sets the property and notifies listeners only when necessary. (!! Without comparing by Equals) </summary>
       /// <typeparam name="T">Type of the property.</typeparam>
       /// <param name="storage">Reference to a property with both getter and setter.</param>
       /// <param name="value">Desired value for the property.</param>
-      /// <param name="propertyName">Name of the property used to notify listeners.  This
-      /// value is optional and can be provided automatically when invoked from compilers that
-      /// support CallerMemberName.</param>
-      protected void SetPropertyForce<T>(ref T storage, T value, [CallerMemberName] String propertyName = null)
+      /// <param name="propertyName">Name of the property used to notify listeners.  This value is optional and can be provided automatically
+      /// when invoked from compilers that support CallerMemberName.</param>
+      protected void SetPropertyForce<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
       {
          var tmp = storage;
          storage = value;
-         this.OnPropertyChanged(tmp, value, propertyName);
+         OnPropertyChanged(tmp, value, propertyName);
       }
 
-      /// <summary>
-      /// Notifies listeners that a property value has changed.
-      /// </summary>
+      /// <summary> Notifies listeners that a property value has changed. </summary>
       /// <param name="oldValue">old value</param>
       /// <param name="newValue">new value</param>
-      /// <param name="propertyName">Name of the property used to notify listeners.  This
-      /// value is optional and can be provided automatically when invoked from compilers
-      /// that support <see cref="CallerMemberNameAttribute"/>.</param>
-      protected void OnPropertyChanged<T>(T oldValue, T newValue, [CallerMemberName] string propertyName = null) {
-         var eventHandlerEx = this.PropertyChangedEx;
-         if (eventHandlerEx != null)
-            eventHandlerEx(this, new PropertyChangedExEventArgs(propertyName, oldValue, newValue));
-         var eventHandler = this.PropertyChanged;
-         if (eventHandler != null)
-            eventHandler(this, new PropertyChangedEventArgs(propertyName));
+      /// <param name="propertyName">Name of the property used to notify listeners.  This value is optional and can be provided automatically
+      /// when invoked from compilers that support <see cref="CallerMemberNameAttribute"/>.</param>
+      private void OnPropertyChanged<T>(T oldValue, T newValue, [CallerMemberName] string propertyName = null)
+      {
+         OnPropertyChanged(this, new PropertyChangedExEventArgs<T>(propertyName, newValue, oldValue));
+      }
+
+      protected void OnPropertyChanged(string propertyName = null)
+      {
+         OnPropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+      }
+
+      protected void OnPropertyChanged(object sender, PropertyChangedEventArgs ev)
+      {
+         var eventHandler = PropertyChanged;
+         eventHandler?.Invoke(sender, ev);
       }
    }
 }
--- FastMines_uwp/Presentation/Notyfier/PropertyChangedExEventArgs.cs
+++ FastMines_uwp/Presentation/Notyfier/PropertyChangedExEventArgs.cs
@@ -1,21 +1,17 @@
 ﻿using System.ComponentModel;
 
-namespace FastMines.Common {
-    // TODO http://stackoverflow.com/questions/7677854/notifypropertychanged-event-where-event-args-contain-the-old-value
+namespace FastMines.Presentation.Notyfier
+{
+   public class PropertyChangedExEventArgs<T> : PropertyChangedEventArgs
+   {
+      public PropertyChangedExEventArgs(string propertyName, T newValue, T oldValue) :
+         base(propertyName)
+      {
+         NewValue = newValue;
+         OldValue = oldValue;
+      }
 
-    public interface INotifyPropertyChangedEx : INotifyPropertyChanged {
-        event PropertyChangedExEventHandler PropertyChangedEx;
-    }
-
-    public delegate void PropertyChangedExEventHandler(object sender, PropertyChangedExEventArgs e);
-
-    public class PropertyChangedExEventArgs : PropertyChangedEventArgs {
-        public object OldValue { get; private set; }
-        public object NewValue { get; private set; }
-
-        public PropertyChangedExEventArgs(string propertyName, object oldValue, object newValue) : base(propertyName) {
-            OldValue = oldValue;
-            NewValue = newValue;
-        }
-    }
+      public T NewValue { get; }
+      public T OldValue { get; }
+   }
 }
