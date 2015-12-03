6557314712db4a2b2b8670fec3e067e09e259e00
SeregaLBN <SeregaLBN@gmail.com>
2015-12-03 23:32:40 +0200

    C# move star coords algorithm to new file - FigureHelper.cs

---
+++ src/src_common/fmg/common/geom/util/FigureHelper.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace fmg.common.geom.util {
+   public static class FigureHelper {
+      /// <summary> https://en.wikipedia.org/wiki/Regular_polygon
+      /// Вернёт координаты правильного многоугольника
+      /// Центр фигуры - координаты [0,0]
+      /// </summary>
+      /// <param name="n">edges / vertices</param>
+      /// <param name="radius"></param>
+      /// <param name="offsetAngle">-360° .. 0° .. +360°</param>
+      /// <returns></returns>
+      public static IEnumerable<PointDouble> GetRegularPolygonCoords(int n, double radius, double offsetAngle = 0) {
+         var angle = 2*Math.PI/n; // 360° / n
+         offsetAngle = offsetAngle*Math.PI/180; // to radians
+         return Enumerable.Range(0, n)
+            .Select(x => x*angle + offsetAngle)
+            .Select(x => new PointDouble(radius*Math.Sin(x), radius*Math.Cos(x)));
+      }
+
+      /// <summary> https://en.wikipedia.org/wiki/Star_polygon
+      /// Суть:
+      ///  * два круга - внешний и внутр
+      ///  * на каждом, по периметру - равноудалённые точки. Кол-во точек == кол-ву лучей у звезды
+      ///  * внутр круг повёрнут относительно внешенего на 360° / (кол-во лучей) / 2
+      ///  * и соединяем (т.е. последовательны в массиве) поочерёдно точки с внешнего и внутр круга
+      /// так и получаем звезду
+      /// Центр фигуры - координаты [0,0]
+      /// </summary>
+      /// <param name="rays">the number of corner vertices</param>
+      /// <param name="radiusOut">external radius</param>
+      /// <param name="radiusIn">internal radius</param>
+      /// <param name="offsetAngle">-360° .. 0° .. +360°</param>
+      /// <returns></returns>
+      public static IEnumerable<PointDouble> GetRegularStarCoords(int rays, double radiusOut, double radiusIn, double offsetAngle = 0) {
+         var pointsExt = GetRegularPolygonCoords(rays, radiusOut, offsetAngle);
+         var pointsInt = GetRegularPolygonCoords(rays, radiusIn, offsetAngle + 180.0/rays);
+         return pointsExt.Zip(pointsInt, (p1, p2) => new[] { p1, p2 }).SelectMany(x => x).ToArray();
+      }
+   }
+}
--- src/src_platform_uwp/fmg/uwp/res/img/MosaicsSkillImg.cs
+++ src/src_platform_uwp/fmg/uwp/res/img/MosaicsSkillImg.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Windows.UI.Xaml.Media.Imaging;
 using fmg.common;
 using fmg.common.geom;
+using fmg.common.geom.util;
 using fmg.data.controller.types;
 using Point = Windows.Foundation.Point;
 using Rect = Windows.Foundation.Rect;
@@ -21,96 +22,16 @@ namespace fmg.uwp.res.img
 
       protected override void MakeCoords() {
          double s = Size - Padding * 2; // size inner Square
-         #region classic star
-#if false
-         {
-            var alpha = (2 * Math.PI) / 10;
-            var radius = s / 2;
-            var center = new[] { s / 2, s / 2 }; // star center [x, y]
+         var r1 = s / 2; // external radius
+         var r2 = s / 5; // internal radius
+         var rays = 4 + MosaicGroup.Ordinal(); // rays count
+         _points = FigureHelper.GetRegularStarCoords(rays, r1, r2).Select(c => new Point(c.x, c.y)).ToArray();
 
-            _points = new Point[10];
-            for (var i = 0; i < 10; i++) {
-               var r = radius * (i % 2 + 1) / 2;
-               var omega = alpha * i;
-               _points[i] = new Point((r * Math.Sin(omega)) + center[0], (r * Math.Cos(omega)) + center[1]);
-            }
-
-            // adding offset
-            for (var i = 0; i < _points.Length; i++) {
-               _points[i].X += Padding;
-               _points[i].Y += Padding;
-            }
+         // adding offset
+         for (var i = 0; i < _points.Length; i++) {
+            _points[i].X += Padding + s / 2;
+            _points[i].Y += Padding + s / 2;
          }
-#endif
-         #endregion
-
-         #region star from js code http://stackoverflow.com/questions/14580033/algorithm-for-drawing-a-5-point-star
-#if !true
-         {
-            Func<Point[], double, Point[]> funcRotate2D = (vecArr, byRads) => {
-               var mat = new double[2, 2] {
-                              { Math.Cos(byRads), -Math.Sin(byRads) },
-                              { Math.Sin(byRads), Math.Cos(byRads) }};
-               var result = new Point[vecArr.Length];
-               for (var i = 0; i < vecArr.Length; ++i) {
-                  result[i] = new Point(
-                     mat[0, 0] * vecArr[i].X + mat[0, 1] * vecArr[i].Y,
-                     mat[1, 0] * vecArr[i].X + mat[1, 1] * vecArr[i].Y);
-               }
-               return result;
-            };
-            Func<int, double, Point[][]> funcGenerateStarTriangles = (numPoints, r) => {
-               var triangleBase = r * Math.Tan(Math.PI / numPoints);
-               var triangle = new Point[] {
-                  new Point(0, r)
-                  , new Point(triangleBase / 2, 0)/*, new Point(-triangleBase / 2, 0), new Point(0, r) */};
-               var result = new Point[numPoints][];
-               for (var i = 0; i < numPoints; ++i) {
-                  result[i] = funcRotate2D(triangle, i * (2 * Math.PI / numPoints));
-               }
-               return result;
-            };
-            var radius = s / 2;
-            _points = funcGenerateStarTriangles(5, radius).SelectMany(x => x).ToArray();
-
-            // adding offset
-            for (var i = 0; i < _points.Length; i++) {
-               _points[i].X += Padding + Size / 2;
-               _points[i].Y += Padding + Size / 2;
-            }
-         }
-#endif
-         #endregion
-
-         #region my star calc
-#if true
-         // идея:
-         // * два круга - внешний и внутр
-         // * на каждом, по периметру - равноудалённые точки. Кол-во точек == кол-ву лучей у звезды
-         // * внутр круг повёрнут относительно внешенего на 360° / (кол-во лучей) / 2
-         // * и соединяем (т.е. последовательны в массиве) поочерёдно точки с внешнего и внутр круга
-         // так и получаем звезду
-         {
-            var r1 = s / 2; // external radius
-            var r2 = s / 5; // internal radius
-            var rays = 4 + MosaicGroup.Ordinal(); // rays count
-            var angle = 2 * Math.PI / rays; // 360° / rays
-            var pointsExt = Enumerable.Range(0, rays)
-               .Select(x => x * angle)
-               .Select(x => new Point(r1 * Math.Sin(x), r1 * Math.Cos(x)));
-            var pointsInt = Enumerable.Range(0, rays)
-               .Select(x => x * angle + angle / 2)
-               .Select(x => new Point(r2 * Math.Sin(x), r2 * Math.Cos(x)));
-            _points = pointsExt.Zip(pointsInt, (p1, p2) => new[] { p1, p2 }).SelectMany(x => x).ToArray();
-
-            // adding offset
-            for (var i = 0; i < _points.Length; i++) {
-               _points[i].X += Padding + s / 2;
-               _points[i].Y += Padding + s / 2;
-            }
-         }
-#endif
-         #endregion
 
          base.MakeCoords(); // => Draw();
       }
