ee8ff8a8bec89f038da3b30d4250709568255d57
SeregaLBN <SeregaLBN@gmail.com>
2016-06-04 23:21:43 +0300

    C# - add TEST in solution

---
+++ FastMines_uwp/Test.FastMines.Uwp.MosaicsImg.WBmp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml.Controls;
+using fmg.common;
+using fmg.data.controller.types;
+using fmg.uwp.res.img;
+
+namespace Test.FastMines.Uwp.MosaicsImg.WBmp {
+
+   public sealed partial class MainPage : Page {
+      public MainPage() {
+         InitializeComponent();
+         var hsv = new HSV(SkillImg.BackgroundColor);
+         SkillImg.PropertyChanged += (o, ev) => {
+            switch (ev.PropertyName) {
+            case "Image":
+               //hsv.h = SkillImg.RotateAngle;
+               //SkillImg.BackgroundColor = hsv.toColor();
+               break;
+            }
+         };
+      }
+
+      public MosaicsSkillImg SkillImg => new MosaicsSkillImg(ESkillLevel.eCrazy, 200) {
+         Rotate = true,
+         RedrawInterval = 50,
+         RotateAngleDelta = 5
+      };
+
+   }
+
+}
