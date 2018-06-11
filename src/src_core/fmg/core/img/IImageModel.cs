df05cab52624b6949d98dcf85ad80705024cb979
SeregaLBN <fastminesgmail.com>
2018-06-11 23:44:57 +0300

    C#: add interface for MVC-model

---
+++ src/src_core/fmg/core/img/IImageModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel;
+using fmg.common.geom;
+
+namespace fmg.core.img {
+
+   /// <summary> Image MVC: model.
+   /// Model of image data/properties/characteristics </summary>
+   public interface IImageModel : INotifyPropertyChanged, IDisposable {
+
+      /// <summary> width and height of the displayed part in pixels </summary>
+      Size Size { get; set; }
+
+   }
+
+}
