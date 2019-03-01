2c762e0a13da6d3c1a108886243a6437ba1ee7da
SeregaLBN <fastminesgmail.com>
2019-03-01 17:04:08 +0200

    java android: fix AsyncRUnner

--- src/src_platform_android/fmg/android/utils/AsyncRunner.java
+++ src/src_platform_android/fmg/android/utils/AsyncRunner.java
@@ -9,18 +9,18 @@ public final class AsyncRunner {
     private void AsyncRunner() {}
 
     public static void RunWithDelay(Runnable run, long delayMs) {
-        new Handler(Looper.getMainLooper()).postAtTime(run, delayMs);
+        new Handler(Looper.getMainLooper()).postDelayed(run, delayMs);
     }
 
-    public static void RepeatNoWait(Runnable run, long delay, Supplier<Boolean> cancelation) {
+    public static void Repeat(Runnable run, long delayMs, Supplier<Boolean> cancelation) {
         Runnable[] run2 = { null };
         run2[0] = () -> {
             if (cancelation.get())
                 return; // stop
             run.run();
-            RunWithDelay(run2[0], delay); // repeat
+            RunWithDelay(run2[0], delayMs); // repeat
         };
-        RunWithDelay(run2[0], delay); // start
+        RunWithDelay(run2[0], delayMs); // start
     }
 
 }
