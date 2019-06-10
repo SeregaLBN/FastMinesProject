6037a493e7c6f41e2328deb80c7d54dad6711598
SeregaLBN <fastminesgmail.com>
2019-06-10 21:14:29 +0300

    uwp: unify namespace

--- FastMines_uwp/FastMines.Uwp.Main/MosaicPage.Prototype.xaml.cs
+++ FastMines_uwp/FastMines.Uwp.Main/MosaicPage.Prototype.xaml.cs
@@ -14,7 +14,7 @@ using Fmg.Uwp.Utils;
 using Fmg.Uwp.Mosaic.Xaml;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-namespace FastMines {
+namespace Fmg {
 
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
