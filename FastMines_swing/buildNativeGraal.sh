5155e98b42a3a3160aebfdfd22556182e34b551d
SeregaLBN <fastminesgmail.com>
2019-12-06 05:50:01 +0200

    Swing-Native: update scripts

--- FastMines_swing/buildNativeGraal.sh
+++ FastMines_swing/buildNativeGraal.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
 
+# Steps:
+# 1. Run app with native-image-agent
+#   (build *.json files into ./src/main/resources/META-INF/native-image/FastMinesGame/FastMines_swing/)
+#   (Click through the entire application and exit)
+# 2. Edit native configuration
+#    ./src/main/resources/META-INF/native-image/FastMinesGame/FastMines_swing/native-image.properties
+# 3. Run make native
+#    see gradle task :FastMines_swing:fastMinesSwingGraalNative
+
+
 JAVA_HOME=$GRAALVM_HOME
 PATH=$GRAALVM_HOME/bin:$PATH 
 
-$GRAALVM_HOME/bin/gu install native-image
-
-
 gradle --stop
+gradle clean
 
+
+# Step 1
+gradle :FastMines_swing:build
+java -agentlib:native-image-agent=config-output-dir=./src/META-INF/native-image/FastMinesGame/FastMines_swing/ \
+     -jar ./build/libs/FastMines_swing-fat-2.1.1.jar
+
+# Step 2
+# see ./src/main/resources/META-INF/native-image/FastMinesGame/FastMines_swing/native-image.properties
+
+# Step 3
+$GRAALVM_HOME/bin/gu install native-image
 gradle clean :FastMines_swing:fastMinesSwingGraalNative --info
